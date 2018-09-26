cba02cd9ef9f20316e2a8bf7086efbc5e48fe222
[flesch] <[friedl.lesch@telekom.de]>
2018-09-26 12:44:47 +0200

    Matchcounter Movecounter restartbutton done and win alert in progress

--- memory/js/app.js
+++ memory/js/app.js
@@ -1,14 +1,17 @@
-
-
 var turnPoint=3;
-var x=0;
-
-
 
 $(".card").click(turn());
 
     turn()
 
+function restart(){
+    console.log("New Game");
+    $(".card").removeClass("match show unclickable")
+    $(".moves").text("0");
+    matchCounter=0;
+    turnpoint=3;
+    turn();
+}
 
 function turn() {
     var flippedCard1 = flippedCard1;
@@ -39,19 +42,26 @@ case 0:
             $(faName).parent().removeClass("open").addClass("match");
             flippedCard1 = undefined;
             flippedCard2 = undefined;
-            moves=moves+1;;
+            moves=moves+1;
             $(".moves").text(moves);
-            turnPoint=0;
             console.log(matchCounter=matchCounter+1);
-            return (matchCounter);
+            // The game ends once all cards have been correctly matched with 16 cards it is matchCounter = 8.
+            if (matchCounter==8){
+                matchCounter=0;
+                win();
+            }
+            else{
+                turnPoint=0;
+            return (matchCounter, moves);
     }
+}
             // What happens when cards do not match
             else if ((flippedCard1.substr(3)) !== (flippedCard2.substr(3))) {
             var faName1 = '.' + (flippedCard1.substr(3));
             var faName2 = '.' + (flippedCard2.substr(3));
             console.log("DISMATCH " + faName1, faName2);
             setTimeout(() => {
-            $(faName1).parent().removeClass("open show unclickable"),
+            $(faName1).parent().removeClass("open show unclickable");
             $(faName2).parent().removeClass("open show unclickable");
             }, 1000);
             flippedCard1 = undefined;
@@ -59,6 +69,7 @@ case 0:
             moves=moves+1;
             $(".moves").text(moves);
             turnPoint=0;
+            return (moves);
         }
     }
 })
@@ -66,117 +77,50 @@ case 0:
 
     break;
 
-
-/*case 1:
-
-    secondFlipp(flippedCard1);
-    break;
-*/
-
-case 2:
-    //    What happens when cards match
-    if ((flippedCard1.substr(3)) === (flippedCard2.substr(3))) {
-        var faName = '.' + (flippedCard1.substr(3));
-        console.log("MATCH " + faName);
-        $(faName).parent().removeClass("open").addClass("match");
-        flippedCard1 = "";
-        flippedCard2 = "";
-        moves=+1;;
-        $(".moves").text(moves);
-        turnPoint=0;
-        return (matchCounter=+1);
-    }
-// What happens when cards do not match
-else if ((flippedCard1.substr(3)) !== (flippedCard2.substr(3))) {
-    var faName1 = '.' + (flippedCard1.substr(3));
-    var faName2 = '.' + (flippedCard2.substr(3));
-    console.log("DISMATCH " + faName1, faName2);
-    setTimeout(() => {
-        $(faName1).parent().removeClass("open show unclickable"),
-        $(faName2).parent().removeClass("open show unclickable");
-    }, 1000);
-    flippedCard1 === ""
-    flippedCard2 === ""
-    moves=+1;
-    $(".moves").text(moves);
-    turnPoint=0;
-    //return(flippedCard1, flippedCard2)
-}
-    break;
-
 case 3:
-    //    turn start
+    //    Gamestart & restart
+    $(".card").removeClass("match show  unclickable");
     var flippedCard1 = "";
     var flippedCard2 = "";
+    matchCounter = 0;
     turnPoint=0;
     break;
 
+case 4:
+    win();
+    break;
 
 default:
     break;
 }
 }
-    function firstFlipp() {
 
-        $(".card").click(function () {
-            if (flippedCard1===""){
-            $(this).addClass("open show unclickable");
-            flippedCard1 = ($(this).children().attr('class'));
-            turnPoint=1;
-            turn(flippedCard1);
-        }   
-            else{
-            $(this).addClass("open show unclickable"),
-            flippedCard2 = ($(this).children().attr('class'));
-            console.log("click 2 " + $(this).children().attr('class'));
-            turnPoint=2;
-            turn(flippedCard1, flippedCard2)
-            }})
-    
+    function win() {
+        
+        //TODO winAlert.html as window.open 
+        
+        //alert(winAlert.html);
+        matchCounter=0;
+        console.log('You Win!')
+        };
+
+    function starColor(moves){
+    if (moves = 8){
+        $( "#star1" ).toggleClass( "stars-highlight" );
     }
-
-    function checkMatch(flippedCard1, flippedCard2) {
-        if ((flippedCard1.substr(3)) === (flippedCard2.substr(3))) {
-            var faName = '.' + (flippedCard1.substr(3));
-            console.log("MATCH " + faName);
-            $(faName).parent().removeClass("open").addClass("match");
-            (flippedCard1).length = 0;
-            flippedCard2 = ""
-            moves=+1;
-            $(".moves").text(moves);
-            turnPoint=0;
-            return (matchCounter=+1, flippedCard1, flippedCard2);
-        }
-    // What happens when cards do not match
-    else if ((flippedCard1.substr(3)) !== (flippedCard2.substr(3))) {
-        var faName1 = '.' + (flippedCard1.substr(3));
-        var faName2 = '.' + (flippedCard2.substr(3));
-        console.log("DISMATCH " + faName1, faName2);
-        setTimeout(() => {
-            $(faName1).parent().removeClass("open show unclickable"),
-            $(faName2).parent().removeClass("open show unclickable");
-        }, 1000);
-        flippedCard1 === ""
-        flippedCard2 === ""
-        moves=+1;
-        $(".moves").text(moves);
-        turnPoint=0;
-        return(flippedCard1, flippedCard2)
+    if (moves = 16){
+        $( "#star2" ).toggleClass( "stars-highlight" );
     }
-};
-
-    function win(matchCounter) {
-        if (matchCounter === 8) {
-            console.log("You Win!")
-        }
-};
-
-
-
+    if (moves = 24){
+        $( "#star3" ).toggleClass( "stars-highlight" );
+    }
+}
+    function scoreLog(){
+        
+    }
 
 /*
 
     If the cards match, both cards stay flipped over.
     If the cards do not match, both cards are flipped face down.
-
-The game ends once all cards have been correctly matched.*/
+*/
