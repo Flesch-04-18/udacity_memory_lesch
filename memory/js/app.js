3c3b3c393debb3369ef1584d8cf368145c822cac
[flesch] <[friedl.lesch@telekom.de]>
2018-10-15 17:14:21 +0200

    cleanup, timer added, final

--- memory/js/app.js
+++ memory/js/app.js
@@ -3,20 +3,50 @@ var cardArray = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane
     "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
 
 var moves = 0;
-var time = "";
+var intervalID = window.setInterval(runTime, 1000);
+var m = 0;
+var s = 0;
+const playTime = $(".time");
 
 $(".card").click();
-restart()
-turn()
+
+restart();
+turn();
+
+function runTime() {
+    //  Insert every second, minutes(m) and seconds(s) in time
+    playTime.text(sPlus1());
+
+    function sPlus1() {
+        if (s < 59) {
+            if (s < 9) {
+                return (m + "\:" + "0" + (s += 1));
+            }
+
+            return (m + "\:" + (s += 1));
+        } if (s = 60) {
+            m += 1;
+            s = (s + 1) - 60;
+            return (m + "\:" + "0" + (s));
+        }
+    };
+}
 
 function restart() {
     //    Gamestart & restart
     matchCounter = 0;
+    s = 0;
+    m = 0;
+    playTime.text("0:00");
+    intervalID = 1000;
+
 
     moves = 0;
     const allCards = document.querySelectorAll(".card");
     $(".fa-star").removeClass("stars-highlight")
     $(".moves").text("0");
+    $("button").addClass("hidden");
+    $(".restart").removeClass("hidden");
     mixcards(cardArray);
 
     for (i = 0; i < 16; i++) {
@@ -25,6 +55,7 @@ function restart() {
             "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb");
         allCards[i].firstElementChild.classList.add(cardArray[i]);
     }
+    turn();
 
 }
 
@@ -57,10 +88,9 @@ function turn() {
             turnpoint += 1;
             $(this).addClass("open show unclickable");
             flippedCard1 = ($(this).children().attr('class'));
-
         }
         // The player then turns over a second card, trying to find the corresponding card with the same symbol.
-        else if (flippedCard2 === undefined && turnpoint === 1 & !this.classList.contains("unclickable")) {
+        if (flippedCard2 === undefined && $("li.open").length === 1 && turnpoint === 1 & !this.classList.contains("unclickable")) {
             turnpoint = 0;
             $(this).addClass("open show unclickable"),
                 flippedCard2 = ($(this).children().attr('class'));
@@ -75,15 +105,13 @@ function turn() {
                 matchCounter += 1;
                 countMovesColorStars()
 
-                // The game ends once all cards have been correctly matched with 16 cards it is matchCounter = 8.
-
-                // set to 1 for testing
-                if (matchCounter === 1) {
+                // The game ends once all cards have been correctly matched with 16 cards it is matchCounter = 8
+                if (matchCounter === 8) {
                     matchCounter = 0;
                     win();
                 }
 
-            }
+            }//
             // What happens when cards do not match
             else if ((flippedCard1.substr(3)) !== (flippedCard2.substr(3))) {
                 var faName1 = '.' + (flippedCard1.substr(3));
@@ -110,37 +138,61 @@ function turn() {
     }
 
     function win() {
+        //open winmessage
 
-        //TODO create modal 
-
-        //open modal win;
         var deck = document.querySelector(".deck");
         var childFromDeck = document.querySelector("li.card");
         var winAllert = document.createElement("div");
-        //const allCards = document.querySelectorAll(".card");
-        var text = "\<h1\>Congratulations, you won\!\!\!\<\/h1\>\<br\>\<br\>"+
-                    "\<h2\>It takes you " + moves + " moves to complete!\<br\>\<br\>" +
-                    "And your Time is " + time + "\<\/h2\>";
 
-        winAllert.insertAdjacentHTML('afterbegin', text);;
-    for (i = 0; i < 16; i++) {
-        var childFromDeck = document.querySelector("li.card");
-        deck.removeChild(childFromDeck);
-        //allCards[i].classList.add("hidden");
-    }
-    deck.appendChild(winAllert)
+        var text = "\<h1\>Congratulations, you won\!\!\!\<\/h1\>\<br\>\<br\>" +
+            "\<h2\>It takes you " + moves + " moves to complete!\<br\>\<br\>" +
+            "And your Time is " + $(".time").text() + "\<\/h2\>\<br\>\<br\>";
+
+        intervalID = 0;
+        $(".time").addClass("hidden");
+        $(".restart").addClass("hidden");
+        $("button").removeClass("hidden");
+        winAllert.insertAdjacentHTML('afterbegin', text);
+
+        for (i = 0; i < 16; i++) {
+            var childFromDeck = document.querySelector("li.card");
+            deck.removeChild(childFromDeck);
+        }
+        deck.appendChild(winAllert);
+    };
 }
 
-    function starColor() {
-        // check moves and add class stars-highlight to stars at 8, 16 and 24 moves
-        if (moves == 8) {
-            $("#star1").addClass("stars-highlight");
-        }
-        if (moves == 16) {
-            $("#star2").addClass("stars-highlight");
-        }
-        if (moves == 24) {
-            $("#star3").addClass("stars-highlight");
-        }
+function starColor() {
+    // check moves and add class stars-highlight to stars at 8, 16 and 24 moves
+    if (moves == 8) {
+        $("#star1").addClass("stars-highlight");
     }
+    if (moves == 16) {
+        $("#star2").addClass("stars-highlight");
+    }
+    if (moves == 24) {
+        $("#star3").addClass("stars-highlight");
+    }
+}
+
+function playAgain() {
+    // is fired when button is clicked
+    // remove winmessage, create new 16 cards in deck
+    var deck = document.querySelector(".deck");
+    var childFromDeck = deck.querySelector("div");
+
+    deck.removeChild(childFromDeck);
+    for (i = 0; i < 16; i++) {
+        var newCard = document.createElement("li");
+        var newI = document.createElement("i");
+        newCard.appendChild(newI);
+        deck.appendChild(newCard);
+    }
+    $("li").addClass("card");
+    const allCards = document.querySelectorAll(".card");
+    for (i = 0; i < 16; i++) {
+        allCards[i].firstElementChild.classList.add("fa");
+    };
+    $(".time").removeClass("hidden");
+    restart();
 }
